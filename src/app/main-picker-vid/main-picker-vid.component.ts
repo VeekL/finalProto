30ea28466a44dfd2fda0b3a75132c4b53140e3f9
Caffa <wangyeelin@gmail.com>
2018-05-21 09:57:50 +0800

    Meh card view

--- src/app/main-picker-vid/main-picker-vid.component.ts
+++ src/app/main-picker-vid/main-picker-vid.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataServiceService} from './../data-service.service';
+
 
 @Component({
   selector: 'app-main-picker-vid',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-picker-vid.component.css']
 })
 export class MainPickerVidComponent implements OnInit {
+  public vidList = [];
 
-  constructor() { }
+  constructor(private _vidListService: DataServiceService) { }
 
   ngOnInit() {
+    this.vidList = this._vidListService.getVidList();
   }
 
 }
